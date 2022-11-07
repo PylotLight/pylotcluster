68569a66bb229da9d2200756b6501bf07efeccee
Light <lochois@gmail.com>
2022-11-07 21:53:36 +1100

    add vars file

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable k8s_host {
+  type        = string
+  default     = "https://127.0.0.1:6443"
+  description = "Cluster host API Endpoint"
+}
+
+variable environment {
+  type        = string
+  default     = ""
+  description = "The current environment for the deployment"
+}
