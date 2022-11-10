5bfaec1cd4dea497eb34016d035a31ff64a22b0c
Light <lochois@gmail.com>
2022-11-10 22:07:28 +1100

    vault init attempt

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.15.0"
     }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.10.0"
+    }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,18 @@ variable "k8s_host" {
   type        = string
   default     = ""
   description = "Cluster host API Endpoint"
+  sensitive   = true
 }
 
 variable "environment" {
   type        = string
   default     = ""
   description = "The current environment for the deployment"
+}
+
+variable "VAULT_ADDR" {
+  type        = string
+  default     = ""
+  description = "description"
+  sensitive   = true
 }
