a18d24f94990a0602489b00357543352a1b09340
Light <lochois@gmail.com>
2022-11-15 21:31:19 +1100

    add test namespace

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,3 +1,9 @@
 provider "kubernetes" {
   host = data.vault_generic_secret.cluster_info.data["k8s_host"]
 }
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
